b00c34391c87456db50ad7dad165730d6db1cbdc
AlexeyRyabov04 <ryabovalexey04@gmail.com>
2024-11-19 23:48:21 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            return await _context.Authors
+                .OrderByDescending(author => author.Books.Max(book => book.Title.Length))
+                .ThenBy(author => author.Id)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await _context.Authors
+                .Where(author => author.Books.Count(book => book.PublishDate.Year > 2015) > 0
+                 && author.Books.Count(book => book.PublishDate.Year > 2015) % 2 == 0)
+                .ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return await _context.Books
+                .OrderByDescending(book => book.QuantityPublished * book.Price)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _context.Books
+                .Where(book => book.Title.Contains("Red") && book.PublishDate > new DateTime(2012, 5, 25))
+                .ToListAsync();
+        }
+    }
+}
